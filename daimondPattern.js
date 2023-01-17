4a2015a83a84bf13943f1985efbc93cfb7a87b72
Rohit Kumar Sain <121244078+RohitKumarSain@users.noreply.github.com>
2023-01-17 10:33:37 +0530

    Update and rename daimondPatternPrint.js to daimondPattern.js

--- daimondPattern.js
+++ daimondPattern.js
@@ -1,22 +1,22 @@
 for (let i = 1; i <= 7; i++) {
-  let row = '';
+  let row = "";
   for (let j = 1; j <= 7; j++) {
     if (j >= 8 - i && j <= i) {
-      row += '*';
+      row += "*";
     } else {
-      row += ' ';
+      row += " ";
     }
   }
   console.log(row);
 }
 
 for (let i = 6; i >= 1; i--) {
-  let row = '';
+  let row = "";
   for (let j = 1; j <= 7; j++) {
     if (j >= 8 - i && j <= i) {
-      row += '*';
+      row += "*";
     } else {
-      row += ' ';
+      row += " ";
     }
   }
   console.log(row);
